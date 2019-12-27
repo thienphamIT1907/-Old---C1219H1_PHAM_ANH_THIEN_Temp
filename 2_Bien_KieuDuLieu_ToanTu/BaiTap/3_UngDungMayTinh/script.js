946fd4c47566715a3e8e01c8168c245814c3ebc5
thienphamIT1907 <phamanhthien1907@gmail.com>
2019-12-27 19:57:26 +0700

    Hoan thanh bai tap 3: Ung dung may tinh

--- 2_Bien_KieuDuLieu_ToanTu/BaiTap/3_UngDungMayTinh/script.js
+++ 2_Bien_KieuDuLieu_ToanTu/BaiTap/3_UngDungMayTinh/script.js
@@ -1,32 +1,31 @@
-let firstNum = Number(document.getElementById('firstNum').value);
-let secondNum = Number(document.getElementById('secondNum').value);
-
-
-function additionBtn(){
-    let firstNum = Number(document.getElementById('firstNum').value);
-    let secondNum = Number(document.getElementById('secondNum').value);
-    let result = firstNum + secondNum;
-    document.getElementById('result').innerHTML = 'result: ' + result ;
+function getFirstNum() {
+    let data = document.getElementById('firstNum').value;
+    return parseFloat(data);
 }
+
+function getSecondNum() {
+    let data = document.getElementById('secondNum').value;
+    return parseFloat(data);
+}
+
+function additionBtn() {
+    let result = getFirstNum() + getSecondNum();
+    document.getElementById('result').innerHTML = 'Result: '+result;
+}
+
 function subtractionBtn() {
-    let firstNum = Number(document.getElementById('firstNum').value);
-    let secondNum = Number(document.getElementById('secondNum').value);
-    let result = firstNum - secondNum;
-    document.getElementById('result').innerHTML = 'result: ' + result ;
+    let result = getFirstNum() - getSecondNum();
+    document.getElementById('result').innerHTML = 'Result: '+result;
 }
+
 function multiplicationBtn() {
-    let firstNum = Number(document.getElementById('firstNum').value);
-    let secondNum = Number(document.getElementById('secondNum').value);
-    let result = firstNum * secondNum;
-    document.getElementById('result').innerHTML = 'result: ' + result ;
+    let result = getFirstNum() * getSecondNum();
+    document.getElementById('result').innerHTML = 'Result: '+result;
 }
+
 function divisionBtn() {
-
-    let firstNum = Number(document.getElementById('firstNum').value);
-    let secondNum = Number(document.getElementById('secondNum').value);
-    let result = firstNum / secondNum;
-    if(result == 'Infinity' || result == '-Infinity') {
-        document.getElementById('result').innerHTML = 'result: ' + 'Error, cannot /0' ;
-    } else document.getElementById('result').innerHTML = 'result: ' + result ;
-
+    let result = getFirstNum() / getSecondNum();
+    if(result =='Infinity' || result == '-Infinity') {
+        document.getElementById('result').innerHTML = 'Result: Cannot divide with 0';
+    } else document.getElementById('result').innerHTML = 'Result: '+result;
 }
